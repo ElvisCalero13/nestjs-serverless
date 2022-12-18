93d8cea969027fff96509ef10e18c9505238ee4b
ElvisCalero13 <elviscaleromanueles@gmail.com>
2022-12-18 17:00:13 -0500

    Update studentController Unit Test

--- src/student/student.controller.spec.ts
+++ src/student/student.controller.spec.ts
@@ -1,16 +1,62 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Test, TestingModule } from '@nestjs/testing';
-import { StudentDynamo } from 'src/db/student/crud-student';
-import { v4 as uuid } from 'uuid';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
-import Student from './entities/student.entity';
 import { StudentController } from './student.controller';
 import { StudentService } from './student.service';
 
 describe('StudentController', () => {
   let controller: StudentController;
-  let spyService: StudentService;
+  const mockService = {
+    findAll: jest.fn().mockResolvedValue([
+      {
+        id: '1',
+        dni: '0919484170',
+        name: 'Elvis',
+        lastname: 'Calero',
+        career: 'System Engineer',
+        level: 10,
+      },
+      {
+        id: '2',
+        dni: '0919484121',
+        name: 'Alex',
+        lastname: 'Calero',
+        career: 'Designer',
+        level: 6,
+      },
+    ]),
+    findOne: jest.fn().mockImplementation((id: string) =>
+      Promise.resolve({
+        id,
+        dni: '0919484170',
+        name: 'Elvis',
+        lastname: 'Calero',
+        career: 'System Engineer',
+        level: 10,
+      }),
+    ),
+    create: jest
+      .fn()
+      .mockImplementation((dto: CreateStudentDto) =>
+        Promise.resolve({ id: 'abc123', ...dto }),
+      ),
+    update: jest
+      .fn()
+      .mockImplementation((id: string, dto: UpdateStudentDto) =>
+        Promise.resolve({ id, dni: '0919484170', ...dto }),
+      ),
+    remove: jest.fn().mockImplementation((id: string) =>
+      Promise.resolve({
+        id,
+        dni: '0919484170',
+        name: 'Elvis',
+        lastname: 'Calero',
+        career: 'System Engineer',
+        level: 10,
+      }),
+    ),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,95 +64,115 @@ describe('StudentController', () => {
       providers: [StudentService],
     })
       .overrideProvider(StudentService)
-      .useValue(spyService)
+      .useValue(mockService)
       .compile();
 
     controller = module.get<StudentController>(StudentController);
   });
-  // beforeAll(async () => {
-  //   const ApiServiceProvider = {
-  //     provide: StudentService,
-  //     useFactory: () => ({
-  //       create: jest.fn((dto: CreateStudentDto) => {
-  //         const result: Student = {
-  //           id: uuid(),
-  //           ...dto,
-  //         };
-  //         return result;
-  //       }),
-  //       findAll: jest.fn(() => []),
-  //       findOne: jest.fn(() => {}),
-  //       update: jest.fn((id: string, dto: UpdateStudentDto) => {
-  //         const result: Student = {
-  //           id: id,
-  //           dni: '',
-  //           name: dto.name,
-  //           lastname: dto.lastname,
-  //           career: dto.career,
-  //           level: dto.level,
-  //         };
-  //         return result;
-  //       }),
-  //       remove: jest.fn(() => {}),
-  //     }),
-  //   };
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     controllers: [StudentController],
-  //     providers: [StudentService, ApiServiceProvider, StudentDynamo],
-  //   }).compile();
 
-  //   controller = module.get<StudentController>(StudentController);
-  //   spyService = module.get<StudentService>(StudentService);
-  // });
-
-  it('calling create method', () => {
-    const dto: CreateStudentDto = {
-      dni: '0919484170',
-      name: 'Elvis',
-      lastname: 'Calero',
-      career: 'System Engineer',
-      level: 6,
-    };
-    expect(controller.create(dto)).not.toEqual(null);
-    expect(controller.create(dto)).toEqual({
-      id: expect.any(String),
-      dni: dto.dni,
-      name: dto.name,
-      lastname: dto.lastname,
-      career: dto.career,
-      level: dto.level,
-    });
-    expect(spyService.create).toHaveBeenCalled();
-    expect(spyService.create).toHaveBeenCalledWith(dto);
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
   });
 
-  it('calling update method', () => {
-    const id: string = uuid();
-    const dto: UpdateStudentDto = {
-      name: 'Elvis',
-      lastname: 'Calero',
-      career: 'System Engineer',
-      level: 6,
-    };
-    expect(controller.update(id, dto)).not.toEqual(null);
-    expect(controller.update(id, dto)).toEqual({
-      id: id,
-      dni: expect.any(String),
-      name: dto.name,
-      lastname: dto.lastname,
-      career: dto.career,
-      level: dto.level,
+  describe('findAll', () => {
+    it('should get an array of students', () => {
+      expect(controller.findAll()).resolves.toEqual([
+        {
+          id: '1',
+          dni: '0919484170',
+          name: 'Elvis',
+          lastname: 'Calero',
+          career: 'System Engineer',
+          level: 10,
+        },
+        {
+          id: '2',
+          dni: '0919484121',
+          name: 'Alex',
+          lastname: 'Calero',
+          career: 'Designer',
+          level: 6,
+        },
+      ]);
+      expect(mockService.findAll).toHaveBeenCalled();
+      expect(mockService.findAll).toHaveBeenCalledTimes(1);
     });
   });
 
-  it('calling findAll method', () => {
-    controller.findAll();
-    expect(spyService.findAll).toHaveBeenCalled();
+  describe('findOne', () => {
+    it('should get a single student', () => {
+      expect(controller.findOne('1')).resolves.toEqual({
+        id: '1',
+        dni: '0919484170',
+        name: 'Elvis',
+        lastname: 'Calero',
+        career: 'System Engineer',
+        level: 10,
+      });
+      expect(mockService.findOne).toHaveBeenCalled();
+      expect(mockService.findOne).toHaveBeenCalledTimes(1);
+      expect(mockService.findOne).toHaveBeenCalledWith('1');
+    });
   });
 
-  it('calling findOne method', () => {
-    const id = '3789';
-    controller.findOne(id);
-    expect(spyService.findOne).toHaveBeenCalled();
+  describe('create', () => {
+    it('should create a new student', () => {
+      const dto: CreateStudentDto = {
+        dni: '0954025474',
+        name: 'Veronica',
+        lastname: 'Macias',
+        career: 'Teacher',
+        level: 8,
+      };
+      expect(controller.create(dto)).resolves.toEqual({
+        id: 'abc123',
+        dni: '0954025474',
+        name: 'Veronica',
+        lastname: 'Macias',
+        career: 'Teacher',
+        level: 8,
+      });
+      expect(mockService.create).toHaveBeenCalled();
+      expect(mockService.create).toHaveBeenCalledTimes(1);
+      expect(mockService.create).toHaveBeenCalledWith(dto);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a student', () => {
+      const dto: UpdateStudentDto = {
+        //dni: '0919484170',
+        name: 'Elvis Fabian',
+        lastname: 'Calero Manueles',
+        career: 'System Engineer',
+        level: 7,
+      };
+      expect(controller.update('1', dto)).resolves.toEqual({
+        id: '1',
+        dni: '0919484170',
+        name: 'Elvis Fabian',
+        lastname: 'Calero Manueles',
+        career: 'System Engineer',
+        level: 7,
+      });
+      expect(mockService.update).toHaveBeenCalled();
+      expect(mockService.update).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a student', () => {
+      expect(controller.remove('1')).resolves.toEqual({
+        id: '1',
+        dni: '0919484170',
+        name: 'Elvis',
+        lastname: 'Calero',
+        career: 'System Engineer',
+        level: 10,
+      });
+      expect(mockService.remove).toHaveBeenCalled();
+      expect(mockService.remove).toHaveBeenCalledTimes(1);
+      expect(mockService.remove).toHaveBeenCalledWith('1');
+    });
   });
 });
